fa8e5360ce631e861365273b1858afde4c781f20
lalaladi <diandabukke2808@gmail.com>
2023-12-08 12:49:19 +0700

    add files

---
+++ application/views/welcome_message.php
@@ -0,0 +1,89 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-image: url('https://cdn.pixabay.com/photo/2023/10/06/10/55/toddler-8297939_1280.jpg');
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: red;
+		background-color: transparent;
+		font-weight: normal;
+		text-decoration: none;
+	}
+
+	a:hover {
+		color: #97310e;
+	}
+
+	h1 {
+		color: yellow;
+		text-align : center;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 30px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	#body {
+		margin: 0 15px 0 15px;
+		min-height: 96px;
+	}
+
+	p {
+		margin: 0 0 10px;
+		padding:0;
+		color: white;
+		font-size: 15px;
+	}
+
+	#end {
+		margin: 0 15px 0 15px;
+		min-height: 96px;
+		text-align : center;
+		font-size: 20px;
+		font-weight: normal;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>HELLO !!</h1>
+	<h1>Welcome to CodeIgniter!</h1>
+</div>
+	<div id="body">
+		<p>This simple page is generated dynamically by CodeIgniter.</p>
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
+		<p>Codelgniter sebuah framework PHP ringan yang cukup efektif untuk mengembangkan website atau aplikasi dengan mudah dan gratis.</p>
+		<p>Framework ini dapat kamu temukan dan  cari di berbagai sumber.</p>
+		<p>link download Codelgniter <a href="https://codeigniter.com/download/">Download</a>.</p>
+	</div>
+	<div id="end">
+		<p>This made by me.</p>
+		<p>Klik Looking for more.</p>
+	</div>
+
+</body>
+</html>
